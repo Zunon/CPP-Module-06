ae8e8b210e1f4f5b560d5a45bf911843735c9953
kalmheir <zunon@zunon.io>
2023-09-16 14:51:34 +0400

    feat: solve ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalmheir <kalmheir@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 14:48:20 by kalmheir          #+#    #+#             */
+/*   Updated: 2023/09/16 14:51:24 by kalmheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+
+Base * generate(void) {
+    srand(time(NULL));    
+    int i = rand() % 3;
+    if (i == 0)
+        return (new A());
+    else if (i == 1)
+        return (new B());
+    else
+        return (new C());
+}
+
+void identify(Base *p) {
+    if (dynamic_cast<A *>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base &p) {
+    if (dynamic_cast<A *>(&p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B *>(&p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C *>(&p))
+        std::cout << "C" << std::endl;
+}
+
+int main(void) {
+    Base *p = generate();
+    identify(p);
+    identify(*p);
+    return (0);
+}
